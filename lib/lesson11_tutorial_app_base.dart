eac9f31e2c42612bb3dc474dcbac3edb857af366
chunsen.zhou <1041883243@qq.com>
2023-11-19 21:19:38 +0800

    feat: add FavoriteWidget for lesson11_tutorial :tada:

--- lib/lesson11_tutorial_app_base.dart
+++ lib/lesson11_tutorial_app_base.dart
@@ -1,11 +1,60 @@
 /*
  * @Author: 周长升
  * @Date: 2023-04-22 12:01:38
- * @LastEditTime: 2023-04-22 12:54:21
+ * @LastEditTime: 2023-07-02 21:01:41
  * @LastEditors: 周长升
  * @Description: 
  */
 import 'package:flutter/material.dart';
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _isFavorited = false;
+        _favoriteCount -= 1;
+      } else {
+        _isFavorited = true;
+        _favoriteCount += 1;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? const Icon(Icons.star) : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+}
 
 class Lesson11TutorialAppBaseDemo extends StatelessWidget {
   Lesson11TutorialAppBaseDemo({super.key});
@@ -32,11 +81,7 @@ class Lesson11TutorialAppBaseDemo extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41')
+          const FavoriteWidget()
         ],
       ));
 
